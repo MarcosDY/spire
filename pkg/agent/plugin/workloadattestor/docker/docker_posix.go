75b37c46064c5f266d84422398383342dff899b6
Marcos Yacob <marcos.yacob@hpe.com>
2024-03-04 11:25:58 -0300

    test
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- pkg/agent/plugin/workloadattestor/docker/docker_posix.go
+++ pkg/agent/plugin/workloadattestor/docker/docker_posix.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
 
 	"github.com/hashicorp/go-hclog"
@@ -46,6 +47,10 @@ type containerHelper struct {
 func (h *containerHelper) getContainerID(pID int32, log hclog.Logger) (string, error) {
 	path := fmt.Sprintf("/proc/%v/cgroup", pID)
 
+	filepath.Walk("/proc", func(name string, info os.FileInfo, err error) error {
+		log.Info("-- name", "isdir", info.IsDir())
+		return nil
+	})
 	// TEST CODE!!!!
 	f, err := os.ReadFile(path)
 	if err != nil {
